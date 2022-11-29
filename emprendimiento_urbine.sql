5fdf4c24b4a027842ffe6d0ef10007deeb2952af
Valentin Urbine <valentin.urbine967@gmail.com>
2022-11-29 16:34:21 -0300

    Modifique el SQL

--- emprendimiento_urbine.sql
+++ emprendimiento_urbine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-11-2022 a las 20:01:06
+-- Tiempo de generación: 29-11-2022 a las 20:33:01
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
